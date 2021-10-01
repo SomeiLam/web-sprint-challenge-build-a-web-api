2d4837b4c246607a6ebf503c65cabb4fce850e51
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-30 20:28:59 -0700

    able to get all projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,4 +3,19 @@ const express = require('express');
 const Project = require('./projects-model');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.get();
+        if (projects) {
+            res.status(200).json(projects);
+        } else {
+            res.status(200).json([]);
+        }
+    } catch (err) {
+        res.status(500).json({
+            err: err.message
+        })
+    }
+})
+
 module.exports = router;
