e3d7489c2c55920980ab92e3bce0c46e5906198c
Amy Lam <amy@Marcos-MBP.hsd1.ca.comcast.net>
2021-09-30 20:51:04 -0700

    able to update project

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -37,7 +37,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const { name, description, completed } = req.body;
+        const { name, description } = req.body;
         if (!name || !description) {
             res.status(400).json({
                 message: "Please provide name and description for the project"
@@ -45,6 +45,30 @@ router.post('/', async (req, res) => {
         } else {
             const newProject = await Project.insert(req.body);
             res.status(201).json(newProject);
+        }
+    } catch (err) {
+        res.status(500).json({
+            err: err.message
+        })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const project = await Project.get(req.params.id)
+        if (!project) {
+            res.status(404).json({
+                message: `Project with id ${req.params.id} not found`
+            })
+        } else {
+            if (!req.body.name || !req.body.description || req.body.completed === undefined) {
+                res.status(400).json({
+                    message: "The request body is missing the required fields"
+                })
+            } else {
+                const updatedProject = await Project.update(req.params.id, req.body);
+                res.status(200).json(updatedProject);
+            }
         }
     } catch (err) {
         res.status(500).json({
